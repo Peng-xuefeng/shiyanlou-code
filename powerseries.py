f01201a4274735cab0e9bb79df0f113e1d1dc91f
Peng-xuefeng <xf.peng@foxmail.com>
2019-09-29 20:18:18 +0800

    计算幂级数

---
+++ powerseries.py
@@ -0,0 +1,13 @@
+#计算幂级数 e ** x = 1 + x + x**2 / 2! + x**3 / 3 ! +  x ** n / n! (x>0 and x<1) 
+#这里需要掌握的技巧就是发现后一项是前一项的 x/n 倍
+result = 1.0
+n = 1
+term = 1
+x = float(input("Please input the number of x:"))
+while n <= 100:
+    term = term * x / n   #用来计算当前项是多少
+    result = result + term
+    n = n + 1
+    if term < 0.0001:
+        break
+print("number is {} result is {}".format(n,result))
